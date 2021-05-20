536a44f97216388235f219aea72aa1b1413970f5
Xavier Mirabelli-Montan <xavier@whiteskyweb.com>
2021-05-21 00:15:31 +0100

    Updating pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,24 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { Box } from 'theme-ui'
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/Layout"
+import Seo from "../components/Seo"
+import SearchList from "../components/SearchList"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <Box
+      sx={{
+        maxWidth: 1200,
+        mx: 'auto',
+        px: 3,
+        py: 4,
+        textAlign: 'center'
+      }}>
+      <SearchList />
+    </Box>
   </Layout>
 )
 

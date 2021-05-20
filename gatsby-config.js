ee992f0a71c6d0e26f28a44fd0375973e9227d59
Xavier Mirabelli-Montan <xavier@whiteskyweb.com>
2021-05-21 00:06:49 +0100

    Adding plugins to gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pokedex`,
+    description: `All your pokemons in one place`,
+    author: `@xaviemirmon`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,18 +19,29 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Pokedex`,
+        short_name: `Pokedex`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Pokeball-Transparent.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-theme-ui`,
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: "Pokemon",
+        fieldName: "allPokemon",
+        url: `https://graphql-pokemon2.vercel.app`
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
